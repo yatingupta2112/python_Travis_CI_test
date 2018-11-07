411c3c700a02729336630acaedd6b47a69eaa1e4
prithvisekhar <prithvisekhar@gmail.com>
2018-11-07 13:30:37 +0530

    initial commit - palindrome code

---
+++ SimplePalindrom.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file. Simple code to check palindrome or not
+Created on Wed June 24 10:56:13 2018
+
+@author: prithvisekhp
+"""
+array = []
+palindrome_input = input("Input word to check for palindrome :")
+divide2 = len(palindrome_input)
+input_size = len(palindrome_input)
+counter = 0
+for j in range(input_size):
+    array.append(palindrome_input[j])
+size_array = len(array)
+for i in range(divide2):
+    if array[i] != array[size_array-1-i]:
+        counter = counter + 1
+if counter == 0:
+    print("It's a palindrome\n")
+else:
+    print("It's not a palindrome\n")
